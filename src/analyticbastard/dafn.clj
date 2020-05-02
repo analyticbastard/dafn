e6a83967f51e410302ab37b3a4385774ab068c87
analyticbastard <albrecht.allenstein@gmail.com>
2020-05-02 04:11:49 +0200

    final version

--- src/analyticbastard/dafn.clj
+++ src/analyticbastard/dafn.clj
@@ -1,28 +1,29 @@
 (ns analyticbastard.dafn
   (:require [clojure.core.async :as async]))
 
-(def in-ch (async/chan 1))
+(def ch (async/chan 1))
+(def sub-c (async/pub ch :route))
 
 (defmacro dafn [fname args & body]
-  (let [d2 `(defn ~fname [~@args]
+  (let [values (gensym 'values)
+        fun-name (str fname)
+        d2 `(defn ~fname [~@args]
               (let [p# (promise)]
-                (println p#)
-                (async/go (async/>!! user/in-ch p#))
-                (loop [args# ~args]
-                  (let [v# (first args#)]
-                    (when v#
-                      (println "sent" v#)
-                      (async/go (async/>!! user/in-ch v#))
-                      (recur (rest args#)))))
+                (println ~fname (keyword ~fname))
+                (async/go (async/>!! ch 
+                                     {:route ~fname :data (cons p# ~args)}))
                 p#))
         d3 `(async/go-loop []
-              (println "start")
-              (let [p# (async/<! user/in-ch)]
+              (let [in-ch# (async/chan 1)
+                    _# (async/sub sub-c ~fname in-ch#)
+                    recv# (:data (async/<! in-ch#))
+                    p# (first recv#)
+                    ~values (rest recv#)]
+                (println "xxx" ~fname  ~values)
                 (deliver p#
-                         (let ~(into [] (zipmap args
-                                                    (map (fn [_] '(async/<! user/in-ch)) args)))
-                           (println "received" ~@args)
-                           (println ~(str args) ~args ~(str body))
+                         (let ~(into [] (interleave args
+                                                    (map-indexed (fn [i _] `(get ~values ~i)) args)))
+                           (println "body" ~body ~@body)
                            ~@body)))
               (recur))]
     [d2 d3]))
